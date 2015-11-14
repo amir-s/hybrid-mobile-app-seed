765c51ceac8335ac3e21ec8a132f8ea297f1a533
Amir Saboury <amir.saboury@gmail.com>
2015-11-14 10:39:38 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat')
+
+// gulp.task('combine', function() {
+//   gulp.src([
+//     'public/lib/angular/angular.min.js',
+//     'public/lib/angular-ui-router/angular-ui-router.min.js',
+//     'public/lib/bootstrap/bootstrap.min.js'
+//   ])
+//   .pipe(concat('bundle.js'))
+//   .pipe(gulp.dest('public/'));
+// });
+
+gulp.task('minify', function() {
+  gulp.src([
+      'app/*.js',
+      'app/services/*.js',
+      'app/factories/*.js',
+      'app/filters/*.js',
+      'app/directives/*.js',
+      'app/controllers/*.js',
+      'app/configs/*.js'
+  ])
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('www/'));
+});
+
+
+gulp.task('watch', ['minify'], function() {
+  gulp.watch([
+    'app/**/*.js',
+    'app/*.js'
+  ],[
+    'minify'
+  ]);
+});
+
+
+gulp.task('default', ['watch']);
